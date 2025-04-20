77014e054fd1127534cacb1a81b7555738dfd433
Badar-Ur-Zaman <hbrsadiqian123@gmail.com>
2025-04-20 20:02:11 +0500

    Added given data

--- task1.py
+++ task1.py
@@ -1,2 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+#given data
+pt = 1
+dh = np.arange(20, 70, 5)
+dv = 2
+n = np.array([3, 3.5, 4]) 
+N = 10000  # number of samples
+variance = 1 # rayleigh fading variance is 1
