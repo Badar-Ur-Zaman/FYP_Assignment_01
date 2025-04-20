88ba4da4fbac85e02476823f38d9c5f4d7ddbbf5
Badar-Ur-Zaman <hbrsadiqian123@gmail.com>
2025-04-20 20:30:32 +0500

    Implemented each part of formula

--- task1.py
+++ task1.py
@@ -7,3 +7,11 @@ dv = 2
 n = np.array([3, 3.5, 4]) 
 N = 10000  # number of samples
 variance = 1 # rayleigh fading variance is 1
+
+alpha = variance/2
+x = np.random.randn(N)
+y = np.random.randn(N)
+c = x + 1j*y
+g = np.sqrt(alpha) * np.abs(c)
+g = np.abs(g)**2 # g is the rayleigh fading channel gain
+d = np.sqrt(dh**2 + dv**2)
