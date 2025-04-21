6e06f2c98b5eccd60f1607432449971e3a46b318
Badar-Ur-Zaman <hbrsadiqian123@gmail.com>
2025-04-21 11:14:12 +0500

    Added users and BS

--- task2.py
+++ task2.py
@@ -1,2 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Part 01 - Network Model: 100 Users and 1 Base Station
+
+area_size = 500
+num_users = 100
+
+# Generate uniformly distributed x and y coordinates for users
+x_users = np.random.uniform(0, area_size, num_users)
+y_users = np.random.uniform(0, area_size, num_users)
